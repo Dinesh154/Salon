a9b43798b858b763095f81874a842b2b360025db
Dinesh154 <dineshkumarreddyb4@gmail.com>
2024-02-08 11:05:48 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:saolon/screens/spalshscreen.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 void main() => runApp(MyApp());
 
@@ -16,117 +17,11 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Splash(),
     );
   }
 }
 
-class CustomTimePicker extends StatefulWidget {
-  final Function(DateTime) onTimeSelected;
-
-  CustomTimePicker({required this.onTimeSelected});
-
-  @override
-  _CustomTimePickerState createState() => _CustomTimePickerState();
-}
-
-class _CustomTimePickerState extends State<CustomTimePicker> {
-  static const double _intervalHeight = 100;
-  static const double _intervalSpacing = 5;
-  static const double _containerHeight = _intervalHeight + _intervalSpacing;
-  static const int _numIntervals = 24;
-
-  double _containerPosition = 0;
-  double _maxContainerPosition =
-      (_numIntervals - 1) * (_intervalHeight + _intervalSpacing);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Expanded(
-          child: Stack(
-            children: [
-              ListView.builder(
-                  itemCount: _numIntervals,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Positioned(
-                        top: index * (_intervalHeight + _intervalSpacing),
-                        child: Container(
-                          height: 70,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(
-                                width: 1,
-                                color: Colors.grey[300]!,
-                              ),
-                            ),
-                          ),
-                          child: Center(
-                            child: Text(
-                              _formatTime(DateTime.utc(0, 0, 0, index)),
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ));
-                  }),
-              Positioned(
-                top: _containerPosition,
-                child: GestureDetector(
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 180.0),
-                    child: Container(
-                      height: _containerHeight,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).accentColor,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.drag_handle,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  onVerticalDragUpdate: (DragUpdateDetails details) {
-                    setState(() {
-                      _containerPosition += details.delta.dy;
-                      _containerPosition =
-                          _containerPosition.clamp(0.0, _maxContainerPosition);
-                      widget.onTimeSelected(
-                          _getTimeForPosition(_containerPosition));
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  DateTime _getTimeForPosition(double position) {
-    final int minutesPerInterval =
-        60 * _intervalHeight ~/ (_intervalHeight + _intervalSpacing);
-    final int minutes =
-        (position / (_intervalHeight + _intervalSpacing) * minutesPerInterval)
-            .round();
-    return DateTime.utc(0, 0, 0, 0, minutes);
-  }
-
-  String _formatTime(DateTime time) {
-    String hour = (time.hour % 12).toString().padLeft(2, '0');
-    String minute = time.minute.toString().padLeft(2, '0');
-    String period = time.hour < 12 ? 'AM' : 'PM';
-    return '$hour:$minute $period';
-  }
-}
 
 class MyHomePage extends StatelessWidget {
   final String title;
